8a8db52250441608dc9c624297cba2c98ddebed6
Russell Jones <jonesrussell42@gmail.com>
2015-08-11 09:58:20 -0400

    Experimental development, hoisting steps

--- app/scripts/svgjs_anim/development.js
+++ app/scripts/svgjs_anim/development.js
@@ -6,21 +6,47 @@ SVGjsAnim.prototype.Development = function() {
     .group()
     .attr('id', 'development');
 
-  this.skip = this.draw.skip(0, 0);
-  this.development.add(this.skip);
-
   // IE doesn't read image size
   var w = this.origSceneW;
   var h = this.origSceneH;
 
-  this.dumpTruck = this.draw.image('images/truck.svg', w, h);
-  this.scene.add(this.dumpTruck);
+  this.scooptramTimmins2 = this.draw.image('images/scooptram_timmins2.svg', w, h);
+  this.scene.add(this.scooptramTimmins2);
 
-/*  this.dumpTruck = this.draw.image('images/truck.svg', w, h);
-  this.scene.add(this.dumpTruck);
+  this.groundSupport = this.draw.image('images/ground_support.svg', w, h);
+  this.scene.add(this.groundSupport);
 
-  this.dumpTruck = this.draw.image('images/truck.svg', w, h);
-  this.scene.add(this.dumpTruck);*/
+  this.jumboDrill = this.draw.image('images/jumbo_drill.svg', w, h);
+  this.scene.add(this.jumboDrill);
+
+  var svgjsanim = this;
+  this.headings.development = this.draw
+    .use('Development_Video_1_', 'images/headings.svg')
+    .move(-150, -667)
+    .click(function(){
+      console.log('development');
+      console.log(svgjsanim.scene.transform());
+      console.log(this);
+      svgjsanim.scene
+        .move(-3700, -1100)
+        .scale(4);
+      console.log(svgjsanim.scene.transform());
+    });
+  this.scene
+    .add(this.headings.development);
+
+  this.headings.mucking = this.draw
+    .use('Mucking', 'images/headings.svg')
+    .move(-150, -667)
+    .click(function(){
+      svgjsanim.scene
+        .move(-3700, -1100)
+        .scale(5);
+    });
+  this.scene
+    .add(this.headings.mucking);
+
+
 
   return this;
 };
