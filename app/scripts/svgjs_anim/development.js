07f713c694f25a312c451354c3ed42454e6b0215
Russell Jones <jonesrussell42@gmail.com>
2015-08-12 14:06:27 -0400

    Add zoom in/out to Development

--- app/scripts/svgjs_anim/development.js
+++ app/scripts/svgjs_anim/development.js
@@ -1,5 +1,5 @@
 'use strict';
-/*global SVGjsAnim */
+/*global SVGjsAnim, SVG */
 
 SVGjsAnim.prototype.Development = function() {
   this.development = this.draw
@@ -19,20 +19,46 @@ SVGjsAnim.prototype.Development = function() {
   this.jumboDrill = this.draw.image('images/jumbo_drill.svg', w, h);
   this.scene.add(this.jumboDrill);
 
-  var svgjsanim = this;
+  var anim = this;
 
   this.headings.development = this.draw
     .use('Development_Video_1_', 'images/headings.svg')
     .move(-150, -667)
+    .addClass('zoom-in')
     .click(function(){
-      console.log(svgjsanim.scene.transform());
-      svgjsanim.scene
-        .animate()
-        .transform({ scaleX: 4.9, scaleY: 4.9, cx: 1240, cy: 690 });
-      console.log(svgjsanim.scene.transform());
-    });
+      console.log('clicked Development');
+      if (this.hasClass('zoom-in')) {
+        this.removeClass('zoom-in')
+          .addClass('action');
+        anim.scene
+          .animate()
+          .transform({ scaleX: 4.9, scaleY: 4.9, cx: 1240, cy: 690 })
+          .after(function(){
+            anim.scene.addClass('zoom-out')
+            console.log('zoomed');
+          })
+        ;
+      } else {
+        console.log('play video');
+      }
+      event.stopPropagation();
+    })
+  ;
   this.scene
     .add(this.headings.development);
+
+  this.scene.click(function(){
+    console.log('clicked Scene');
+    if (this.hasClass('zoom-out')) {
+      this.removeClass('zoom-out');
+      this.animate()
+      .transform(new SVG.Matrix);
+
+      anim.headings.development
+        .removeClass('action')
+        .addClass('zoom-in');
+    }
+  });
 
   return this;
 };
