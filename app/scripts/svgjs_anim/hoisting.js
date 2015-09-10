ebf7b32f836ca8fd04a6134ab1f9a176e6a2ce04
Russell Jones <jonesrussell42@gmail.com>
2015-09-10 11:21:36 -0400

    Rock breaker from master.vg

--- app/scripts/svgjs_anim/hoisting.js
+++ app/scripts/svgjs_anim/hoisting.js
@@ -14,7 +14,7 @@ SVGjsAnim.prototype.Hoisting = function() {
   this.skip = this.draw.skip(0, 0);
   this.elevator = this.draw.elevator();
   this.dumpTruck = this.draw.image('images/truck.svg', w, h);
-  this.rockBreaker = this.draw.image('images/rock_breaker.svg', w, h);
+  this.rockBreaker = this.draw.use('Rock_Breaker', 'images/master.svg');
 
   this.hoisting
     .add(this.dumpTruck)
@@ -25,7 +25,6 @@ SVGjsAnim.prototype.Hoisting = function() {
   this.elevator.go();
   this.skip.go();
 //    this.truck.go();
-//    this.rockBreaker.go();
 
   return this;
 };
