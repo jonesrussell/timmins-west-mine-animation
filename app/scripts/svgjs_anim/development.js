073229876a2234247ac06656b1bcf3422dcd8f4c
Russell Jones <jonesrussell42@gmail.com>
2015-09-16 15:17:07 -0400

    Ground Support loop animation

--- app/scripts/svgjs_anim/development.js
+++ app/scripts/svgjs_anim/development.js
@@ -21,17 +21,19 @@ SVGjsAnim.prototype.Development = function() {
 
   // Ground Support Bits
   this.gsBits = this.draw.group();
-  var gsBit1  = this.draw.use('GS_Bit_8', 'images/master.svg').opacity(0);
-  var gsBit2  = this.draw.use('GS_Bit_9', 'images/master.svg').opacity(0);
-  var gsBit3  = this.draw.use('GS_Bit_10', 'images/master.svg').opacity(0);
-  var gsBit4  = this.draw.use('GS_Bit_11', 'images/master.svg').opacity(0);
-  var gsBit5  = this.draw.use('GS_Bit_12', 'images/master.svg').opacity(0);
-  var gsBit6  = this.draw.use('GS_Bit_13', 'images/master.svg').opacity(0);
-  var gsBit7  = this.draw.use('GS_Bit_14', 'images/master.svg').opacity(0);
-  var gsBit8  = this.draw.use('GS_Bit_15', 'images/master.svg').opacity(0);
-  var gsBit9  = this.draw.use('GS_Bit_16', 'images/master.svg').opacity(0);
-  var gsBit10  = this.draw.use('GS_Bit_17', 'images/master.svg').opacity(0);
-  var gsBit11  = this.draw.use('GS_Bit_18', 'images/master.svg').opacity(0);
+  var gsBit1  = this.draw.use('GS_Bit_6', 'images/master.svg').opacity(0);
+  var gsBit2  = this.draw.use('GS_Bit_7', 'images/master.svg').opacity(0);
+  var gsBit3  = this.draw.use('GS_Bit_8', 'images/master.svg').opacity(0);
+  var gsBit4  = this.draw.use('GS_Bit_9', 'images/master.svg').opacity(0);
+  var gsBit5  = this.draw.use('GS_Bit_10', 'images/master.svg').opacity(0);
+  var gsBit6  = this.draw.use('GS_Bit_11', 'images/master.svg').opacity(0);
+  var gsBit7  = this.draw.use('GS_Bit_12', 'images/master.svg').opacity(0);
+  var gsBit8  = this.draw.use('GS_Bit_13', 'images/master.svg').opacity(0);
+  var gsBit9  = this.draw.use('GS_Bit_14', 'images/master.svg').opacity(0);
+  var gsBit10  = this.draw.use('GS_Bit_15', 'images/master.svg').opacity(0);
+  var gsBit11  = this.draw.use('GS_Bit_16', 'images/master.svg').opacity(0);
+  var gsBit12  = this.draw.use('GS_Bit_17', 'images/master.svg').opacity(0);
+  var gsBit13  = this.draw.use('GS_Bit_18', 'images/master.svg').opacity(0);
   this.gsBits
     .add(gsBit1)
     .add(gsBit2)
@@ -44,14 +46,16 @@ SVGjsAnim.prototype.Development = function() {
     .add(gsBit9)
     .add(gsBit10)
     .add(gsBit11)
+    .add(gsBit12)
+    .add(gsBit13)
   ;
   this.development.add(this.gsBits);
 
   // Ground Support Truck
   this.gsTruck = this.draw.groundSupport(this.gsBits)
-    .x(-30)
     .attr('id', 'gs-truck');
   this.development.add(this.gsTruck);
+  this.gsTruck.go();
 
   this.jumboDrill = this.draw.image('images/jumbo_drill.svg', w, h);
   this.development.add(this.jumboDrill);
