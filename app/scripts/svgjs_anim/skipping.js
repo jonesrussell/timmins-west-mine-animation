09df7b651e56f6f369f9555f6c686920ffc1f2b2
Russell Jones <jonesrussell42@gmail.com>
2015-09-29 10:49:48 -0400

    Restore skipping heading

--- app/scripts/svgjs_anim/skipping.js
+++ app/scripts/svgjs_anim/skipping.js
@@ -5,6 +5,10 @@ SVGjsAnim.prototype.Skipping = function() {
   this.skipping = this.draw
     .group()
     .attr('id', 'skipping');
+  this.scene.add(this.skipping);
+
+  this.headings.skipping = this.Heading('ORE__x26__WASTE_SKIPPING_Video', 'skipping', 4.9, 690, 700);
+  this.sceneHeadings.add(this.headings.skipping);
 
   return this;
 };
