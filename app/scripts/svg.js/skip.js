7a00095bd7ec601c44f3ace13b2eb849b4e9e440
Russell Jones <jonesrussell42@gmail.com>
2015-09-29 15:39:38 -0400

    Added chain and gates; formatting

--- app/scripts/svg.js/skip.js
+++ app/scripts/svg.js/skip.js
@@ -6,39 +6,57 @@ SVG.Skip = SVG.invent({
     inherit: SVG.G,
     extend: {
        build: function() {
-         this.attr('id', 'animation-skipping');
-         this.startX = 0;
-         this.startY = 0;
-         this.duration = 20000;
-         this.skipTo = -470;
+          this.attr('id', 'animation-skipping');
+          this.startX = 0;
+          this.startY = 0;
+          this.duration = 20000;
+          this.skipTo = -470;
 
-         // Skip
-         this.clip = this.doc()
-           .rect(1366, 700)
-           .move(0, 217);
-         this.add(this.clip);
-         this.skipBody = this.doc()
-           .image('images/skip.svg', 1366, 700)
-           .clipWith(this.clip);
-         this.add(this.skipBody);
+          // Skip
+          this.clip = this.doc()
+            .rect(1366, 700)
+            .move(0, 217);
+          this.add(this.clip);
+          this.skipBody = this.doc()
+            .image('images/skip.svg', 1366, 700)
+            .clipWith(this.clip);
+          this.add(this.skipBody);
 
-         // Ore falling in skipping
-         this.rocksClip = this.doc()
-           .rect(10, 10)
-           .move(771, 613)
-         ;
-         this.rocks = this.doc()
-           .use('Rocks_from_ore_bin_to_skip', 'images/master.svg')
-         ;
-         this.rocksWrapper = this.doc()
-           .group()
-           .add(this.rocksClip)
-           .add(this.rocks)
-           .clipWith(this.rocksClip)
-         ;
-         this.add(this.rocksWrapper);
+          // Chain
+          this.chain = this.doc()
+            .use('Loading_Bin_Chain_1_', 'images/master.svg')
+          ;
+          this.add(this.chain);
 
-         return this;
+          // Gate upper
+          this.gateUpper = this.doc()
+            .use('Feed_Chute_Knife_Gate', 'images/master.svg')
+          ;
+          this.add(this.gateUpper);
+
+          // Gate lower
+          this.gateLower = this.doc()
+            .use('Measuring_Flask_Knife_Gate', 'images/master.svg')
+          ;
+          this.add(this.gateLower);
+
+          // Ore falling in skipping
+          this.rocksClip = this.doc()
+            .rect(10, 10)
+            .move(771, 613)
+          ;
+          this.rocks = this.doc()
+            .use('Rocks_from_ore_bin_to_skip', 'images/master.svg')
+          ;
+          this.rocksWrapper = this.doc()
+            .group()
+            .add(this.rocksClip)
+            .add(this.rocks)
+            .clipWith(this.rocksClip)
+          ;
+          this.add(this.rocksWrapper);
+
+          return this;
        }
     },
     construct: {
