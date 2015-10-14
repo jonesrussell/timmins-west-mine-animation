6568b33cbc4a2cc3b735c649de963976e61aedff
Russell Jones <jonesrussell42@gmail.com>
2015-10-14 14:34:42 -0400

    Skipping 90% complete

--- app/scripts/svg.js/skip.js
+++ app/scripts/svg.js/skip.js
@@ -9,7 +9,8 @@ SVG.Skip = SVG.invent({
           this.attr('id', 'animation-skipping');
           this.startX = 0;
           this.startY = 0;
-          this.duration = 20000;
+          this.upDuration = 8000;
+          this.headframeSpillDuration = 2000;
           this.skipTo = -470;
 
           var img = 'images/master.svg';
@@ -32,10 +33,10 @@ SVG.Skip = SVG.invent({
           this.add(this.gateLower);
 
           // Measuring box rocks
-          this.rocks1 = this.doc().use('Measuring_Pocket_Rocks_1', img);
-          this.rocks2 = this.doc().use('Measuring_Pocket_Rocks_2', img);
-          this.rocks3 = this.doc().use('Measuring_Pocket_Rocks_3', img);
-          this.rocks4 = this.doc().use('Measuring_Pocket_Rocks_4', img);
+          this.rocks1 = this.doc().use('Measuring_Pocket_Rocks_1', img).opacity(0);
+          this.rocks2 = this.doc().use('Measuring_Pocket_Rocks_2', img).opacity(0);
+          this.rocks3 = this.doc().use('Measuring_Pocket_Rocks_3', img).opacity(0);
+          this.rocks4 = this.doc().use('Measuring_Pocket_Rocks_4', img).opacity(0);
           this.add(this.rocks1)
             .add(this.rocks2)
             .add(this.rocks3)
@@ -43,10 +44,10 @@ SVG.Skip = SVG.invent({
           ;
 
           // Skip
-          this.rocks5 = this.doc().use('Skip_Rocks_1', img);
-          this.rocks6 = this.doc().use('Skip_Rocks_2', img);
-          this.rocks7 = this.doc().use('Skip_Rocks_3', img);
-          this.rocks8 = this.doc().use('Skip_Rocks_4', img);
+          this.rocks5 = this.doc().use('Skip_Rocks_1', img).opacity(0);
+          this.rocks6 = this.doc().use('Skip_Rocks_2', img).opacity(0);
+          this.rocks7 = this.doc().use('Skip_Rocks_3', img).opacity(0);
+          this.rocks8 = this.doc().use('Skip_Rocks_4', img).opacity(0);
           this.skipRocks = this.doc()
             .group()
             .add(this.rocks5)
@@ -85,6 +86,30 @@ SVG.Skip = SVG.invent({
           ;
           this.add(this.rocksWrapper);
 
+          // Headframe
+          this.headframeRock1 = this.doc().use('Rockpile_from_headframe_1_1_', img).opacity(0);
+          this.headframeRock2 = this.doc().use('Rockpile_from_headframe_2_2_', img).opacity(0);
+          this.headframeRock3 = this.doc().use('Rockpile_from_headframe_3_1_', img).opacity(0);
+          this.headframeRock4 = this.doc().use('Rockpile_from_headframe_4_2_', img).opacity(0);
+          this.add(this.headframeRock1)
+            .add(this.headframeRock2)
+            .add(this.headframeRock3)
+            .add(this.headframeRock4)
+          ;
+
+          this.headframeRocksClip = this.doc()
+            .rect(67, 65)
+            .move(716, 140)
+          ;
+          this.headframeRocks = this.doc().use('Rocks_from_Headframe_to_box', img);
+          this.headframeRocksWrapper = this.doc()
+            .group()
+            .add(this.headframeRocksClip)
+            .add(this.headframeRocks)
+            .clipWith(this.headframeRocksClip)
+          ;
+          this.add(this.headframeRocksWrapper);
+
           return this;
        }
     },
@@ -96,29 +121,205 @@ SVG.Skip = SVG.invent({
 });
 
 SVG.extend(SVG.Skip, {
-    spillRocks: function() {
-      this.rocks
-        .animate(10000)
-        .move(85, 75)
+    reset: function() {
+      this.headframeRock1.animate().opacity(0);
+      this.headframeRock2.animate().opacity(0);
+      this.headframeRock3.animate().opacity(0);
+      this.headframeRock4.animate().opacity(0);
+    }
+    , gateUpperUp: function() {
+      return this.gateUpper.animate(2000).y(0);
+    }
+    , gateUpperDown: function() {
+      return this.gateUpper.animate(2000).y(9);
+    }
+    , gateLowerUp: function() {
+      return this.gateLower.animate(2000).y(0);
+    }
+    , gateLowerDown: function() {
+      return this.gateLower.animate(2000).y(15);
+    }
+    , chainDown: function() {
+      return this.chain.animate(2000).move(0, 0);
+    }
+    , chainUp: function() {
+      return this.chain.animate(2000).move(0, -9);
+    }
+    , measuringPileShow: function() {
+      var self = this;
+      return self.rocks4
+        .animate(500, '<>', 500)
+        .opacity(1)
+        .after(function(){
+          self.rocks3
+            .animate(500)
+            .opacity(1)
+            .after(function(){
+              self.rocks2
+                .animate(500)
+                .opacity(1)
+                .after(function(){
+                  self.rocks1
+                    .animate(500)
+                    .opacity(1)
+                  ;
+                })
+              ;
+            })
+          ;
+        })
       ;
+    }
+    , measuringPileHide: function() {
+      var self = this;
+      return self.rocks1
+        .animate(500, '<>', 500)
+        .opacity(0)
+        .after(function(){
+          self.rocks2
+            .animate(500)
+            .opacity(0)
+            .after(function(){
+              self.rocks3
+                .animate(500)
+                .opacity(0)
+                .after(function(){
+                  self.rocks4
+                    .animate(500)
+                    .opacity(0)
+                  ;
+                })
+              ;
+            })
+          ;
+        })
+      ;
+    }
+    , skipPileShow: function() {
+      var self = this;
+      return self.rocks8
+        .animate(500, '<>', 500)
+        .opacity(1)
+        .after(function(){
+          self.rocks7
+            .animate(500)
+            .opacity(1)
+            .after(function(){
+              self.rocks6
+                .animate(500)
+                .opacity(1)
+                .after(function(){
+                  self.rocks5
+                    .animate(500)
+                    .opacity(1)
+                  ;
+                })
+              ;
+            })
+          ;
+        })
+      ;
+    }
+    , skipPileHide: function() {
+      this.rocks8.opacity(0);
+      this.rocks7.opacity(0);
+      this.rocks6.opacity(0);
+      this.rocks5.opacity(0);
       return this;
     }
-    , go: function() {
-        var self = this;
-        self.spillRocks();
-        self.skipBodyWrapper.animate(this.duration)
-          .move(this.startX, this.skipTo)
-          .after(function(){
-            self.goBack();
-          });
-        return this;
+    , headframePile: function() {
+      var self = this;
+      return this.headframeRock4
+        .animate(500, '<>', 500)
+        .opacity(1)
+        .after(function(){
+          self.headframeRock3
+            .animate(500)
+            .opacity(1)
+            .after(function(){
+              self.headframeRock2
+                .animate(500)
+                .opacity(1)
+                .after(function(){
+                  self.headframeRock1
+                    .animate(500)
+                    .opacity(1)
+                  ;
+                })
+              ;
+            })
+          ;
+        })
+      ;
     }
-    , goBack: function() {
-        var self = this;
-        this.skipBodyWrapper.animate(this.duration)
-          .move(this.startX, this.startY)
-          .after(function(){
-              self.go();
+    , headframeSpillRocks: function() {
+      var self = this;
+      this.headframePile();
+      return this.headframeRocks
+        .animate(this.headframeSpillDuration, '-', 0)
+        .move(-310, 245)
+        .after(function(){
+          self.headframeRocks
+            .move(0, 0)
+          ;
+        })
+      ;
+    }
+    , spillRocks: function() {
+      return this.rocks
+        .animate(2500)
+        .move(85, 75)
+      ;
+    }
+    , spillRocksReset: function() {
+      this.rocks.move(0, 0);
+      return this;
+    }
+    , up: function() {
+        return this.skipBodyWrapper
+          .animate(this.upDuration)
+          .move(this.startX, this.skipTo)
+        ;
+    }
+    , down: function() {
+      return this.skipBodyWrapper
+        .animate(this.upDuration)
+        .move(this.startX, this.startY)
+      ;
+    }
+    , headframeDump: function() {
+      return this.headframeSpillRocks();
+    }
+    , go: function() {
+      var self = this;
+      self.gateUpperDown().after(function(){
+        self.chainUp().after(function(){
+          self.measuringPileShow();
+          self.spillRocks().after(function(){
+            self.spillRocksReset();
+            self.chainDown().after(function(){
+              self.gateUpperUp().after(function(){
+                self.gateLowerDown().after(function(){
+                  self.measuringPileHide();
+                  self.skipPileShow();
+                  self.spillRocks().after(function(){
+                    self.gateLowerUp().after(function(){
+                      self.up().after(function(){
+                        self.skipPileHide();
+                        self.headframeDump();
+                        self.down().after(function(){
+                          self.reset();
+                          self.go();
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
           });
+        });
+      });
+      return this;
     }
 });
