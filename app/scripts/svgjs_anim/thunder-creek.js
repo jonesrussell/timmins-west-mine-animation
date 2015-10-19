518fa1bba11368620bec9bb719e90305c5e18228
Russell Jones <jonesrussell42@gmail.com>
2015-10-19 15:19:04 -0400

    Add long holes to stoping and thunder creek

--- app/scripts/svgjs_anim/thunder-creek.js
+++ app/scripts/svgjs_anim/thunder-creek.js
@@ -15,11 +15,30 @@ SVGjsAnim.prototype.ThunderCreek = function() {
     .add(this.zimbaDrill)
     .add(this.zimbaParts)
   ;
-  this.scene.add(this.zimba);
+  this.thunderCreek.add(this.zimba);
   this.zimba
     .animate(6000)
     .x(100)
     .loop()
+  ;
+
+  /* Longholes */
+  var longholesTC = this.draw.use('TC_Loading_Longholes_Static', 'images/master.svg');
+  var longholesPartsTC = this.draw.use('TC_Loading_Longholes_Parts', 'images/master.svg');
+  this.longholesTCLegs1 = this.draw.use('TC_Loading_Longholes_Legs_1', 'images/master.svg');
+  this.longholesTCLegs2 = this.draw.use('TC_Loading_Longholes_Legs_2', 'images/master.svg');
+  this.longholesTCLegs3 = this.draw.use('TC_Loading_Longholes_Legs_3', 'images/master.svg');
+  this.longholesTCLegs4 = this.draw.use('TC_Loading_Longholes_Legs_4', 'images/master.svg');
+  var longholesTCLegs = this.draw.group()
+    .add(this.longholesTCLegs1)
+    .add(this.longholesTCLegs2)
+    .add(this.longholesTCLegs3)
+    .add(this.longholesTCLegs4)
+  ;
+  this.thunderCreek
+    .add(longholesTC)
+    .add(longholesPartsTC)
+    .add(longholesTCLegs)
   ;
 
   /* Scooptram */
