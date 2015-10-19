7771dd904516ffd8c4f57a4cd8b4c60eaaeea2f1
Russell Jones <jonesrussell42@gmail.com>
2015-10-19 13:33:34 -0400

    Move Haulage TC stuff into own file

--- app/scripts/svgjs_anim/haulage.js
+++ app/scripts/svgjs_anim/haulage.js
@@ -6,28 +6,6 @@ SVGjsAnim.prototype.Haulage = function() {
     .group()
     .attr('id', 'haulage');
 
-  this.zimbaDrill = this.draw.use('TC_Zimba_Longhole_Drill', 'images/master.svg');
-  this.zimbaParts = this.draw.use('TC_Zimba_Longhole_Parts', 'images/master.svg');
-  this.zimba = this.draw
-    .group()
-    .add(this.zimbaDrill)
-    .add(this.zimbaParts)
-  ;
-  this.scene.add(this.zimba);
-  this.zimba
-    .animate(6000)
-    .x(100)
-    .loop()
-  ;
-
-  this.scooptramHaulage = this.draw.scooptram()
-    .move(-700, -42.5)
-    .setX(50)
-  ;
-  this.haulage.add(this.scooptramHaulage);
-  this.scooptramHaulage.go();
-
-
   this.headings.haulage = this.Heading('Haulage_Video', 'haulage', 3.65, 560, 600);
 
   this.scene
