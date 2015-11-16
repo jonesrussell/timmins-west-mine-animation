37d22c75de64b30d7ad4fb85e1db04d31da8a8bd
Russell Jones <jonesrussell42@gmail.com>
2015-11-16 14:45:52 -0500

    Add dump truck to haulage

--- app/scripts/svgjs_anim/haulage.js
+++ app/scripts/svgjs_anim/haulage.js
@@ -4,15 +4,19 @@
 SVGjsAnim.prototype.Haulage = function() {
   this.haulage = this.draw
     .group()
-    .attr('id', 'haulage');
+    .attr('id', 'haulage')
+  ;
 
-  this.headings.haulage = this.Heading('Haulage_Video', 'haulage', 3.65, 560, 600);
+  this.dumpTruckDump = this.draw.dumpTruck();
+  this.haulage.add(this.dumpTruckDump);
+  this.dumpTruckDump.go();
 
   this.scene
-    .add(this.haulage);
+    .add(this.haulage)
+  ;
 
-  this.sceneHeadings
-    .add(this.headings.haulage);
+  this.headings.haulage = this.Heading('Haulage_Video', 'haulage', 3.65, 560, 600);
+  this.sceneHeadings.add(this.headings.haulage);
 
   return this;
 };
