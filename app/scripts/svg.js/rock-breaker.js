1af5b88f4735cd84d2c6911832754d28477929ea
Russell Jones <jonesrussell42@gmail.com>
2015-11-17 10:52:59 -0500

    Split rock breaker rocks from rock breaker

--- app/scripts/svg.js/rock-breaker.js
+++ app/scripts/svg.js/rock-breaker.js
@@ -8,12 +8,14 @@ SVG.RockBreaker = SVG.invent({
         build: function() {
           this.t = 500;
           this.strike = 250;
-          this.rbArm = this.doc().use('Rock_Breaker_Arm', 'images/master.svg');
-          this.rbBit = this.doc().use('Rock_Breaker_Bit', 'images/master.svg');
-          var rbCircle = this.doc().use('Rock_Breaker_Circle', 'images/master.svg');
-          this.add(this.rbArm)
-              .add(this.rbBit)
-              .add(rbCircle);
+
+          this.arm = this.doc().use('Rock_Breaker_Arm', 'images/master.svg');
+          this.bit = this.doc().use('Rock_Breaker_Bit', 'images/master.svg');
+          var circle = this.doc().use('Rock_Breaker_Circle', 'images/master.svg');
+          this.add(this.arm)
+            .add(this.bit)
+            .add(circle)
+          ;
 
           return this;
       }
@@ -25,37 +27,37 @@ SVG.RockBreaker = SVG.invent({
 
 SVG.extend(SVG.RockBreaker, {
     up: function() {
-      this.rbArm
+      this.arm
         .animate(this.t)
         .rotate(-0.02)
         .x(0.1)
         .y(-0.1)
       ;
-      return this.rbBit
+      return this.bit
         .animate(this.t)
         .rotate(-0.05)
       ;
     },
     down: function() {
-      this.rbArm
-        .animate(this.t)
+      this.arm
+        .animate(this.strike)
         .rotate(0.02)
         .x(-0.1)
         .y(0.1)
       ;
-      return this.rbBit
+      return this.bit
         .animate(this.t)
         .rotate(0.05)
       ;
     },
     reset: function() {
-      this.rbArm
+      this.arm
         .animate(this.t)
         .rotate(0)
         .x(0)
         .y(0)
       ;
-      return this.rbBit
+      return this.bit
         .animate(this.t)
         .rotate(0)
       ;
--- app/scripts/svgjs_anim/hoisting.js
+++ app/scripts/svgjs_anim/hoisting.js
@@ -4,19 +4,23 @@
 SVGjsAnim.prototype.Hoisting = function() {
   this.hoisting = this.draw
     .group()
-    .attr('id', 'hoisting');
+    .attr('id', 'hoisting')
+  ;
   this.scene.add(this.hoisting);
 
   this.elevator = this.draw.elevator();
   this.rockBreaker = this.draw.rockBreaker();
+  this.rockBreakerRocks = this.draw.rockBreakerRocks();
 
   this.hoisting
     .add(this.rockBreaker)
+    .add(this.rockBreakerRocks)
     .add(this.elevator)
   ;
 
   this.elevator.go();
   this.rockBreaker.go();
+  this.rockBreakerRocks.go();
 
   return this;
 };
